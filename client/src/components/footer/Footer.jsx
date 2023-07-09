ae2dea20990ed2694131c58ef6b9cfe0aeb03c69
KhushiSaini7 <94100598+KhushiSaini7@users.noreply.github.com>
2023-07-09 08:04:20 +0530

    frontend

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -44,7 +44,7 @@ const Footer = () => {
           <li className="fListItem">Terms & conditions</li>
         </ul>
       </div>
-      <div className="fText">Copyright © 2023 QuickBook.</div>
+      
     </div>
   );
 };
